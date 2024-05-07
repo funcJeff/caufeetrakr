f2d86821327c1596e42aac6ccb04a67442296951
Jeff Martin <jmtechwork@gmail.com>
2024-05-07 16:29:35 -0500

    Add DrinkListView, CoffeeTrackerView

--- CaufeeTrakr/DrinkListView.swift
+++ CaufeeTrakr/DrinkListView.swift
@@ -5,4 +5,41 @@
 //  Created by Jeff Martin on 5/7/24.
 //
 
-import Foundation
+import SwiftUI
+
+// Display a list of drinks.
+// Users can select drinks from the list.
+struct DrinkListView: View {
+    
+    @EnvironmentObject var coffeeData: CoffeeData
+    @Environment(\.presentationMode) var presentationMode
+    
+    // Layout the view's body.
+    var body: some View {
+        List {
+            // Add a tappable row for each drink.
+            ForEach(DrinkType.allCases) { drinkType in
+                Button(action: { self.addDrink(type: drinkType) }) {
+                    Text(drinkType.name)
+                }
+            }
+        }
+    }
+    
+    // Update the model when the user taps a drink.
+    func addDrink(type: DrinkType) {
+        // Add a drink to the model.
+        coffeeData.addDrink(mgCaffeine: type.mgCaffeinePerServing,
+                            onDate: Date())
+        
+        // Dismiss the view.
+        presentationMode.wrappedValue.dismiss()
+    }
+}
+
+// Configure a preview of the drink list view.
+struct DrinkListView_Previews: PreviewProvider {
+    static var previews: some View {
+        DrinkListView()
+    }
+}
