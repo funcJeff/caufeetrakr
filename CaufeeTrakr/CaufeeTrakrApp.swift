6cc9ee382ae275c093b9a51d046bff6cc4c533be
Jeff Martin <jmtechwork@gmail.com>
2024-05-07 15:48:33 -0500

    Initial Commit

---
+++ CaufeeTrakr/CaufeeTrakrApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaufeeTrakrApp.swift
+//  CaufeeTrakr
+//
+//  Created by Jeff Martin on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CaufeeTrakrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
