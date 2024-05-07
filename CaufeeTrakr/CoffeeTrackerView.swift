0380e281c457d1193b22063966934610a71a33d3
Jeff Martin <jmtechwork@gmail.com>
2024-05-07 16:17:36 -0500

    Add DrinkListView, CoffeeTrackerView

---
+++ CaufeeTrakr/CoffeeTrackerView.swift
@@ -0,0 +1,8 @@
+//
+//  CoffeeTrackerView.swift
+//  CaufeeTrakr
+//
+//  Created by Jeff Martin on 5/7/24.
+//
+
+import Foundation
---
+++ CaufeeTrakr/DrinkListView.swift
@@ -0,0 +1,8 @@
+//
+//  DrinkListView.swift
+//  CaufeeTrakr
+//
+//  Created by Jeff Martin on 5/7/24.
+//
+
+import Foundation
